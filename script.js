9fc14f68af4492cda64caeb757f0bd13b462840f
onlyychristian <168069382+onlyychristian@users.noreply.github.com>
2025-12-25 21:57:41 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function copyIP() {
   navigator.clipboard.writeText("birbsmp.com");
-  alert("Server IP copied: birbsmp.com");
+  alert("Copied: birbsmp.com");
+}
+
+function toggleMenu() {
+  document.getElementById("menu").classList.toggle("show");
 }
